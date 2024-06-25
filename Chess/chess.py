2c7451ec8fd974ad50cf42932ccb115e64aaa715
Nathan Kamprath <44905105+NathanKamprath@users.noreply.github.com>
2024-06-25 19:12:06 -0400

    Update chess.py
    Added some selection tools

--- Chess/chess.py
+++ Chess/chess.py
@@ -38,30 +38,30 @@ valid_moves = []
 #MAKE SURE TO PORT TO A NEW FILE FOR ALL THE IMGS AND PIECE LISTS
 
 #NEED TO SET image.load() LOCATION TO PERSONAL FOLDER LOCATION
-bP = pg.transform.scale(pg.image.load('Games/Chess/images/bP.png'), (80, 80))
+bP = pg.transform.scale(pg.image.load('Minigames/Chess/images/bP.png'), (80, 80))
 #bP_small = pg.transform.scale(pg.image.load('images/bP.png'), (45, 45))
-bR = pg.transform.scale(pg.image.load('Games/Chess/images/bR.png'), (80, 80))
+bR = pg.transform.scale(pg.image.load('Minigames/Chess/images/bR.png'), (80, 80))
 #bR_small = pg.transform.scale(pg.image.load('images/bR.png'), (45, 45))
-bN = pg.transform.scale(pg.image.load('Games/Chess/images/bN.png'), (80, 80))
+bN = pg.transform.scale(pg.image.load('Minigames/Chess/images/bN.png'), (80, 80))
 #bN_small = pg.transform.scale(pg.image.load('images/bN.png'), (45, 45))
-bB = pg.transform.scale(pg.image.load('Games/Chess/images/bB.png'), (80, 80))
+bB = pg.transform.scale(pg.image.load('Minigames/Chess/images/bB.png'), (80, 80))
 #bB_small = pg.transform.scale(pg.image.load('images/bB.png'), (45, 45))
-bQ = pg.transform.scale(pg.image.load('Games/Chess/images/bQ.png'), (80, 80))
+bQ = pg.transform.scale(pg.image.load('Minigames/Chess/images/bQ.png'), (80, 80))
 #bQ_small = pg.transform.scale(pg.image.load('images/bQ.png'), (45, 45))
-bK = pg.transform.scale(pg.image.load('Games/Chess/images/bK.png'), (80, 80))
+bK = pg.transform.scale(pg.image.load('Minigames/Chess/images/bK.png'), (80, 80))
 #bK_small = pg.transform.scale(pg.image.load('images/bK.png'), (45, 45))
 
-wP = pg.transform.scale(pg.image.load('Games/Chess/images/wP.png'), (80, 80))
+wP = pg.transform.scale(pg.image.load('Minigames/Chess/images/wP.png'), (80, 80))
 #wP_small = pg.transform.scale(pg.image.load('images/wP.png'), (45, 45))
-wR = pg.transform.scale(pg.image.load('Games/Chess/images/wR.png'), (80, 80))
+wR = pg.transform.scale(pg.image.load('Minigames/Chess/images/wR.png'), (80, 80))
 #wR_small = pg.transform.scale(pg.image.load('images/wR.png'), (45, 45))
-wN = pg.transform.scale(pg.image.load('Games/Chess/images/wN.png'), (80, 80))
+wN = pg.transform.scale(pg.image.load('Minigames/Chess/images/wN.png'), (80, 80))
 #wN_small = pg.transform.scale(pg.image.load('images/wN.png'), (45, 45))
-wB = pg.transform.scale(pg.image.load('Games/Chess/images/wB.png'), (80, 80))
+wB = pg.transform.scale(pg.image.load('Minigames/Chess/images/wB.png'), (80, 80))
 #wB_small = pg.transform.scale(pg.image.load('images/wB.png'), (45, 45))
-wQ = pg.transform.scale(pg.image.load('Games/Chess/images/wQ.png'), (80, 80))
+wQ = pg.transform.scale(pg.image.load('Minigames/Chess/images/wQ.png'), (80, 80))
 #wQ_small = pg.transform.scale(pg.image.load('images/wQ.png'), (45, 45))
-wK = pg.transform.scale(pg.image.load('Games/Chess/images/wK.png'), (80, 80))
+wK = pg.transform.scale(pg.image.load('Minigames/Chess/images/wK.png'), (80, 80))
 #wK_small = pg.transform.scale(pg.image.load('images/wK.png'), (45, 45))
 
 white_img = [wP, wQ, wK, wN, wR, wB]
@@ -131,6 +131,11 @@ def Draw_Pieces():
 
 
 
+def check_ops():
+    pass
+
+
+
 running = True
 while running:
     timer.tick(fps)
@@ -144,7 +149,49 @@ while running:
             running = False
 
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-            x_crd = event.pos[0] // 100
+            x_coor = event.pos[0] // 100 - 3
+            y_coor = event.pos[1] // 100
+            clk_coor = (x_coor, y_coor)
+
+            #For White
+            if turn_select < 2:
+                if clk_coor in white_loc:
+                    selection = white_loc.index(clk_coor)
+                    if turn_select == 0:
+                        turn_select = 1
+                if clk_coor in valid_moves and selection != 100:
+                    white_loc[selection] = clk_coor
+                    if clk_coor in black_loc:
+                        black_piece = black_loc.index(clk_coor)
+                        captured_white.append(black_pieces[black_piece])
+                        black_pieces.pop(black_piece)
+                        black_loc.pop(black_piece)
+                    
+                    black_ops = check_ops(black_pieces, black_loc, 'black')
+                    white_ops = check_ops(white_pieces, white_loc, 'white')
+                    turn_select = 2
+                    selection = 100
+                    valid_moves = []
+            
+            #For Black
+            if turn_select >= 2:
+                if clk_coor in black_loc:
+                    selection = black_loc.index(clk_coor)
+                    if turn_select == 2:
+                        turn_select = 3
+                if clk_coor in valid_moves and selection != 100:
+                    black_loc[selection] = clk_coor
+                    if clk_coor in white_loc:
+                        white_piece = white_loc.index(clk_coor)
+                        captured_black.append(white_pieces[white_piece])
+                        white_pieces.pop(white_piece)
+                        white_loc.pop(white_piece)
+                    
+                    white_ops = check_ops(white_pieces, white_loc, 'white')
+                    black_ops = check_ops(black_pieces, black_loc, 'black')
+                    turn_select = 0
+                    selection = 100
+                    valid_moves = []
 
     pg.display.flip()
 pg.quit()
